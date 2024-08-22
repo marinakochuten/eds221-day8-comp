e2c146f7c8f2b9631dc289d7284fae96b10178e1
marinakochuten <marinakochuten@gmail.com>
2024-08-22 14:15:45 -0700

    finished interactive sesh

---
+++ patchwork_example.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(patchwork)
+library(here)
+
+lizards <- read.csv(here("data_tidy", "lizards.csv"))
+
+two_lizards <- lizards |>
+  filter(common_name %in% c("eastern fence", "western whiptail"))
+
+p1 <- ggplot(data = two_lizards, aes(x = total_length, y = weight)) +
+  geom_point(aes(color = common_name)) +
+  scale_color_manual(values = c("orange", "navy"),
+                     name = "Lizard species:",
+                     labels = c("Eastern fence lizard",
+                                "Western whiptail lizard")) +
+  theme_minimal() +
+  theme(legend.position = c(0.2, 0.8),
+        legend.background = element_blank()) +
+  labs(x = "Total length (mm)",
+       y = "Weight (grams)")
+
+p2 <- ggplot(data = lizards, aes(x = weight, y = site)) +
+  geom_boxplot() +
+  labs(x = "Weight (grams)", y = "Site")
+
+p3 <- ggplot(data = lizards, aes(x = weight)) +
+  geom_histogram() +
+  labs(x = "Weight (grams)", y = "Counts (n)")
+
+p4 <- (p1 + p2) / p3
+ggsave("patchwork_example.png", p4)
